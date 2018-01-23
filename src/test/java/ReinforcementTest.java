22012da8c72fff4138707d2d3e0e858c1edcfcfe
IdoAgam <teamagam2@gmail.com>
2018-01-23 15:50:39 +0200

    removed obsolete code

--- src/test/java/ReinforcementTest.java
+++ src/test/java/ReinforcementTest.java
@@ -7,21 +7,22 @@ import static org.mockito.Mockito.*;
 
 public class ReinforcementTest {
 
+    private static final int NO_SOLDIERS = 0;
     private int boardDim;
     private Player player1;
     private Player player2;
-    private Game game;
 
     @Before
     public void setUp() {
         boardDim = 3;
         player1 = mock(Player.class);
         player2 = mock(Player.class);
-        game = new Game(boardDim, 20, player1, player2);
     }
 
     @Test
     public void callsPlayerOnReinforcementWithGameBoard() {
+        Game game = new Game(boardDim, NO_SOLDIERS, player1, player2);
+
         game.start();
 
         verify(player1).onReinforcement(eq(game.getBoard()), any(int.class));
@@ -30,8 +31,10 @@ public class ReinforcementTest {
 
     @Test
     public void callsPlayerOnReinforcementWithNumberOfSoldiers() {
-        makePlayer1ControlTotalOf_3_Cells();
-        makePlayer2ControlTotalOf_2_Cells();
+        Game game = new Game(boardDim, NO_SOLDIERS, player1, player2);
+
+        makePlayer1ControlTotalOf_3_Cells(game);
+        makePlayer2ControlTotalOf_2_Cells(game);
 
         game.start();
 
@@ -41,24 +44,26 @@ public class ReinforcementTest {
 
     @Test
     public void appliesReinforcementSimplestCase() {
-        when(player1.onReinforcement(any(Board.class), any(int.class)))
-                .thenReturn(Lists.newArrayList(new ReinforcementMove(1, 1, 1)));
+        int soldiers = 13;
+        Game game = new Game(boardDim, NO_SOLDIERS, player1, player2);
+        game.getBoard().setCell(2, 3, new Cell(2, soldiers));
         when(player2.onReinforcement(any(Board.class), any(int.class)))
-                .thenReturn(Lists.newArrayList(new ReinforcementMove(boardDim, boardDim, 1)));
+                .thenReturn(Lists.newArrayList(new ReinforcementMove(2, 3, 1)));
 
         game.start();
 
         Board board = game.getBoard();
-        TestUtils.assertCellContents(board.getHome1Cell(), 1, 21);
-        TestUtils.assertCellContents(board.getHome2Cell(), 2, 21);
+        TestUtils.assertCellContents(board.getCell(2, 3), 2, soldiers + 1);
     }
 
-    private void makePlayer2ControlTotalOf_2_Cells() {
-        game.getBoard().setCell(3, 2, new Cell(2, 2));
-    }
-
-    private void makePlayer1ControlTotalOf_3_Cells() {
-        game.getBoard().setCell(1, 2, new Cell(1, 4));
+    private void makePlayer1ControlTotalOf_3_Cells(Game game) {
+        game.getBoard().setCell(1, 1, new Cell(1, 4));
+        game.getBoard().setCell(1, 3, new Cell(1, 4));
         game.getBoard().setCell(3, 1, new Cell(1, 19));
+    }
+
+    private void makePlayer2ControlTotalOf_2_Cells(Game game) {
+        game.getBoard().setCell(2, 2, new Cell(2, 2));
+        game.getBoard().setCell(3, 2, new Cell(2, 2));
     }
 }
