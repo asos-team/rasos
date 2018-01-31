6242d1ecb2633d2ed8c47a25c43cacff45372065
IdoAgam <teamagam2@gmail.com>
2018-01-31 15:59:36 +0200

    Improved error messages for assertCellContents

--- src/test/java/TestUtils.java
+++ src/test/java/TestUtils.java
@@ -1,10 +1,9 @@
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class TestUtils {
     static void assertCellContents(Cell cell, int controllingPlayer, int numSoldiers) {
-        assertTrue(cell.isControlledBy(controllingPlayer));
-        assertThat(cell.getNumSoldiers(), is(numSoldiers));
+        assertTrue(String.format("Expected cell to be controlled by %d", controllingPlayer), cell.isControlledBy(controllingPlayer));
+        assertEquals(String.format("Expected cell to have %d soldiers in it", numSoldiers), cell.getNumSoldiers(), numSoldiers);
     }
 }
