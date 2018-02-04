43fa755654b32b807ab0e63c1249825f9f5250cb
Bar <teamagam2@gmail.com>
2018-02-04 15:35:32 +0200

    Attacker ignores illegal moves

--- src/main/java/Attacker.java
+++ src/main/java/Attacker.java
@@ -3,13 +3,37 @@ public class Attacker {
     public void apply(int playerId, Iterable<AttackMove> attackMoves, Board board) {
         for (AttackMove move : attackMoves) {
             if (move != null) {
-                Cell originCell = board.cellAt(move.getOriginCol(), move.getOriginRow());
-                Cell destCell = board.cellAt(move.getDestCol(), move.getDestRow());
-                int amount = move.getAmount();
-                originCell.setNumSoldiers(originCell.getNumSoldiers() - amount);
-                destCell.setNumSoldiers(destCell.getNumSoldiers() + amount);
-                destCell.setControllingPlayerId(playerId);
+                apply(playerId, board, move);
             }
         }
+    }
+
+    private void apply(int playerId, Board board, AttackMove move) {
+        if (isValidMove(playerId, board, move)) {
+            executeMove(playerId, board, move);
+        }
+    }
+
+    private boolean isValidMove(int playerId, Board board, AttackMove move) {
+        return move.getAmount() <= getOriginCell(board, move).getNumSoldiers();
+    }
+
+    private Cell getOriginCell(Board board, AttackMove move) {
+        return board.cellAt(move.getOriginCol(), move.getOriginRow());
+    }
+
+    private void executeMove(int playerId, Board board, AttackMove attackMove) {
+        Cell originCell = getOriginCell(board, attackMove);
+        Cell destCell = getDestCell(board, attackMove);
+
+        int amount = attackMove.getAmount();
+
+        originCell.setNumSoldiers(originCell.getNumSoldiers() - amount);
+        destCell.setNumSoldiers(destCell.getNumSoldiers() + amount);
+        destCell.setControllingPlayerId(playerId);
+    }
+
+    private Cell getDestCell(Board board, AttackMove attackMove) {
+        return board.cellAt(attackMove.getDestCol(), attackMove.getDestRow());
     }
 }
--- src/test/java/AttackTest.java
+++ src/test/java/AttackTest.java
@@ -1,5 +1,6 @@
 import com.google.common.collect.Lists;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Collections;
@@ -49,5 +50,45 @@ public class AttackTest {
 
         TestUtils.assertCellContents(board.getHome1Cell(), 1, 18);
         TestUtils.assertCellContents(board.cellAt(2, 1), 1, 2);
+    }
+
+    @Test
+    public void ignoresExceedingAttackMove() {
+        AttackMove am = new AttackMove(1, 1, 2, 2, 300);
+
+        attacker.apply(1, Collections.singleton(am), board);
+
+        TestUtils.assertCellContents(board.getHome1Cell(), 1, 20);
+        TestUtils.assertCellContents(board.getHome2Cell(), 2, 20);
+    }
+
+    @Test
+    public void ignoreExceedingAttackMovesAndAppliesLegalOnes() {
+        AttackMove legal = new AttackMove(1, 1, 2, 1, 10);
+        AttackMove illegal = new AttackMove(1, 1, 2, 2, 12);
+        AttackMove legal2 = new AttackMove(1, 1, 1, 2, 5);
+
+        attacker.apply(1, Lists.newArrayList(legal, illegal, legal2), board);
+
+        TestUtils.assertCellContents(board.getHome1Cell(), 1, 5);
+        TestUtils.assertCellContents(board.cellAt(2, 1), 1, 10);
+        TestUtils.assertCellContents(board.cellAt(1, 2), 1, 5);
+        TestUtils.assertCellContents(board.getHome2Cell(), 2, 20);
+
+
+    }
+
+    @Ignore
+    @Test
+    public void conqueringAttackMove() {
+        Board b = new Board(2);
+        b.populateHomeBases(10);
+        b.cellAt(1, 1).setNumSoldiers(20);
+        AttackMove am = new AttackMove(1, 1, 2, 2, 15);
+
+        attacker.apply(1, Collections.singleton(am), b);
+
+        TestUtils.assertCellContents(b.getHome1Cell(), 1, 5);
+        TestUtils.assertCellContents(b.getHome2Cell(), 1, 5);
     }
 }
