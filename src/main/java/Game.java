6be457bc634db767d10574b859393e29f14fc911
IdoAgam <teamagam2@gmail.com>
2018-02-08 12:53:20 +0200

    renamed variable in Game (quota)

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -39,9 +39,9 @@ class Game {
     }
 
     private void reinforce(int playerId) {
-        int requiredNumberOfSoldiers = board.getPlayerCellCount(playerId);
-        Iterable<ReinforcementMove> moves = players.get(playerId).onReinforcement(board, requiredNumberOfSoldiers);
-        reinforcer.apply(board, moves, requiredNumberOfSoldiers, playerId);
+        int quota = board.getPlayerCellCount(playerId);
+        Iterable<ReinforcementMove> moves = players.get(playerId).onReinforcement(board, quota);
+        reinforcer.apply(board, moves, quota, playerId);
     }
 
     private Iterable<AttackMove> getAttackMoves(Player player) {
