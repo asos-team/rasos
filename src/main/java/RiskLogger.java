7ccb58a388f1b1a3d5eb646e6cb9b0619ceecadb
Guy Bukchin <sevalor@gmail.com>
2018-02-11 15:08:30 +0200

    Basic StdoutRiskLogger implementation and missing classes

---
+++ src/main/java/RiskLogger.java
@@ -0,0 +1,3 @@
+public interface RiskLogger {
+    void logStart();
+}
---
+++ src/test/java/StdoutRiskLoggerTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class StdoutRiskLoggerTest {
+
+
+    private ByteArrayOutputStream outContent;
+    private StdoutRiskLogger logger;
+
+    @Before
+    public void setUp(){
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        logger = new StdoutRiskLogger();
+    }
+    @Test
+    public void newLoggerIsNotNull() {
+        assertThat(logger, notNullValue());
+    }
+
+    @Test
+    public void logStartPrintsGameStarted() {
+        logger.logStart();
+
+        assertEquals("Game started\r\n",outContent.toString());
+    }
+
+
+}
