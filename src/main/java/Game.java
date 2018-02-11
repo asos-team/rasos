3161ae34925c4c6c3315218c0c75d0a895ea24bd
IdoAgam <teamagam2@gmail.com>
2018-02-11 15:42:40 +0200

    Removed all tests of Reinforcement and Attack from Game (already exists in RoundHandler) and added verification to call the handler.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,55 +1,21 @@
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 class Game {
-    private final Map<Integer, Player> players;
-    private final Reinforcer reinforcer;
-    private final Attacker attacker;
+    private final RoundHandler handler;
     private Board board;
     private RiskLogger logger;
 
-    Game(int dim, int numSoldiers, Player playerA, Player playerB, Attacker attacker, Reinforcer reinforcer,RiskLogger logger) {
-        Board board = new Board(dim);
-        this.players = new HashMap<>(2);
-        players.put(1, playerA);
-        players.put(2, playerB);
-        this.board = board;
-        this.reinforcer = reinforcer;
-        this.attacker = attacker;
+    Game(int dim, int numSoldiers, RoundHandler handler, RiskLogger logger) {
+        this.board = new Board(dim);
+        this.handler = handler;
         this.board.populateHomeBases(numSoldiers);
-        this.logger=logger;
+        this.logger = logger;
     }
 
     void start() {
         logger.logStart();
-        reinforce();
-        attack();
-    }
-
-    private void reinforce() {
-        for (int playerId = 1; playerId <= 2; playerId++) {
-            reinforce(playerId);
-        }
-    }
-
-    private void attack() {
-        attacker.apply(getBoard(), getAttackMoves(players.get(1)), getAttackMoves(players.get(2)));
+        handler.playOneRound(board);
     }
 
     Board getBoard() {
         return board;
-    }
-
-    private void reinforce(int playerId) {
-        int quota = board.getPlayerCellCount(playerId);
-        Iterable<ReinforcementMove> moves = players.get(playerId).onReinforcement(board, quota);
-        reinforcer.apply(board, moves, quota, playerId);
-    }
-
-    private Iterable<AttackMove> getAttackMoves(Player player) {
-        Iterable<AttackMove> attackMoves = player.onAttack(getBoard());
-        attackMoves = attackMoves != null ? attackMoves : Collections.emptyList();
-        return attackMoves;
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -3,27 +3,27 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class GameTest {
 
     private static final int NO_SOLDIERS = 0;
     private int boardDim;
-    private Player playerA;
-    private Player playerB;
     private RiskLogger logger;
+    private Game game;
+    private RoundHandler handler;
 
     @Before
     public void setUp() {
         boardDim = 7;
-        playerA = mock(Player.class);
-        playerB = mock(Player.class);
+        handler = mock(RoundHandler.class);
         logger = mock(RiskLogger.class);
+        game = createSimpleGame(NO_SOLDIERS);
     }
 
     @Test
     public void initializesWithSpecifiedDimension() {
-        Game game = createSimpleGame();
         assertThat(game.getBoard().getDim(), is(boardDim));
     }
 
@@ -37,105 +37,18 @@ public class GameTest {
     }
 
     @Test
-    public void callsPlayerOnReinforcementWithGameBoard() {
-        Game game = createSimpleGame();
-
+    public void callsRoundHandlerWithBoard() {
         game.start();
-
-        verify(playerA).onReinforcement(eq(game.getBoard()), any(int.class));
-        verify(playerB).onReinforcement(eq(game.getBoard()), any(int.class));
+        verify(handler).playOneRound(game.getBoard());
     }
 
     @Test
-    public void callsPlayerOnReinforcementWithNumberOfSoldiers() {
-        Game game = createSimpleGame();
-        makePlayerAControlTotalOf_3_Cells(game);
-        makePlayerBControlTotalOf_2_Cells(game);
-
-        game.start();
-
-        verify(playerA).onReinforcement(any(Board.class), eq(3));
-        verify(playerB).onReinforcement(any(Board.class), eq(2));
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    public void callsReinforcerWithReinforcementMoves() {
-        Reinforcer reinforcer = mock(Reinforcer.class);
-
-        Iterable<ReinforcementMove> movesA = mock(Iterable.class);
-        Iterable<ReinforcementMove> movesB = mock(Iterable.class);
-        when(playerA.onReinforcement(any(Board.class), any(int.class))).thenReturn(movesA);
-        when(playerB.onReinforcement(any(Board.class), any(int.class))).thenReturn(movesB);
-
-        Game game = createSimpleGame(reinforcer, new Attacker());
-        makePlayerAControlTotalOf_3_Cells(game);
-        makePlayerBControlTotalOf_2_Cells(game);
-
-        game.start();
-
-        verify(reinforcer).apply(game.getBoard(), movesA, 3, 1);
-        verify(reinforcer).apply(game.getBoard(), movesB, 2, 2);
-    }
-
-    @Test
-    public void callsPlayerOnAttackWithGameBoard() {
-        Game game = createSimpleGame();
-
-        game.start();
-
-        verify(playerA).onAttack(game.getBoard());
-        verify(playerB).onAttack(game.getBoard());
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    public void callsAttackerWithAttackMoves() {
-        Attacker attacker = mock(Attacker.class);
-
-        Iterable<AttackMove> movesA = mock(Iterable.class);
-        Iterable<AttackMove> movesB = mock(Iterable.class);
-        when(playerA.onAttack(any(Board.class))).thenReturn(movesA);
-        when(playerB.onAttack(any(Board.class))).thenReturn(movesB);
-
-        Game game = createSimpleGame(new Reinforcer(), attacker);
-
-        game.start();
-
-        verify(attacker).apply(game.getBoard(), movesA, movesB);
-    }
-
-    @Test
-    public void gameCallsLogStartOnMatchStart(){
-        Game game = createSimpleGame();
+    public void gameCallsLogStartOnMatchStart() {
         game.start();
         verify(logger).logStart();
     }
 
-    private Game createSimpleGame() {
-        return createSimpleGame(new Reinforcer(), new Attacker());
-    }
-
     private Game createSimpleGame(int soldiers) {
-        return createSimpleGame(new Reinforcer(), new Attacker(), soldiers);
-    }
-
-    private Game createSimpleGame(Reinforcer reinforcer, Attacker attacker) {
-        return createSimpleGame(reinforcer, attacker, NO_SOLDIERS);
-    }
-
-    private Game createSimpleGame(Reinforcer reinforcer, Attacker attacker, int soldiers) {
-        return new Game(boardDim, soldiers, playerA, playerB, attacker, reinforcer,logger);
-    }
-
-    private void makePlayerAControlTotalOf_3_Cells(Game game) {
-        game.getBoard().setCell(1, 1, new Cell(1, 4));
-        game.getBoard().setCell(1, 3, new Cell(1, 4));
-        game.getBoard().setCell(3, 1, new Cell(1, 19));
-    }
-
-    private void makePlayerBControlTotalOf_2_Cells(Game game) {
-        game.getBoard().setCell(2, 2, new Cell(2, 2));
-        game.getBoard().setCell(3, 2, new Cell(2, 2));
+        return new Game(boardDim, soldiers, handler, logger);
     }
 }
