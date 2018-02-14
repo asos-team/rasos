bb19a165ffd777ec2da6d43fc50bb736487917ec
IdoAgam <teamagam2@gmail.com>
2018-02-14 14:50:39 +0200

    Rearranged code

--- src/main/java/rasos/GameEndChecker.java
+++ src/main/java/rasos/GameEndChecker.java
@@ -5,15 +5,15 @@ public class GameEndChecker {
         return playerHaveNoSoldiers(board, 1) || playerHaveNoSoldiers(board, 2);
     }
 
-    private boolean playerHaveNoSoldiers(Board board, int id) {
-        return board.getPlayerCellCount(id) == 0;
-    }
-
     public int getWinnerId(Board board) {
         if (!isEndOfGame(board))
             throw new RuntimeException("The game has not ended, please make sure to check isEndOfGame(board) first.");
         if (!board.isEmpty())
             return board.getPlayerCellCount(1) > 0 ? 1 : 2;
         return 0;
+    }
+
+    private boolean playerHaveNoSoldiers(Board board, int id) {
+        return board.getPlayerCellCount(id) == 0;
     }
 }
