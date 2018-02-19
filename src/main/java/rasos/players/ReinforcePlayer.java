9b897e7aedf8d8c8c2a67f8fe9e111da3e81f041
Bar <teamagam2@gmail.com>
2018-02-19 14:47:07 +0200

    Implemented reinforcement-only player. Guava to prod code!

---
+++ src/main/java/rasos/players/ReinforcePlayer.java
@@ -0,0 +1,27 @@
+package rasos.players;
+
+import com.google.common.collect.Iterables;
+import rasos.*;
+
+import java.util.ArrayList;
+
+public class ReinforcePlayer extends Player {
+    @Override
+    public Iterable<ReinforcementMove> onReinforcement(Board board, int reinforcement) {
+        ArrayList<ReinforcementMove> reinforcementMoves = new ArrayList<>();
+        Iterable<CellCoordinates> myCellsCoordinates = getMyCellsCoordinates(board);
+        int numControlledCells = Iterables.size(myCellsCoordinates);
+
+        for (CellCoordinates cc :
+                myCellsCoordinates) {
+            reinforcementMoves.add(new ReinforcementMove(cc.getColIdx(), cc.getRowIdx(), reinforcement / numControlledCells));
+        }
+
+        return reinforcementMoves;
+    }
+
+    @Override
+    public Iterable<AttackMove> onAttack(Board board) {
+        return null;
+    }
+}
