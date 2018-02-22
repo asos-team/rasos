e915fbff13e5a725232fe71f84a57e717de961c6
IdoAgam <teamagam2@gmail.com>
2018-02-22 15:21:51 +0200

    Inlined variables

--- src/test/java/rasos/JsPlayerTest.java
+++ src/test/java/rasos/JsPlayerTest.java
@@ -8,6 +8,7 @@ import org.junit.rules.ExpectedException;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.mock;
+import static rasos.JsPlayer.*;
 
 public class JsPlayerTest {
     @Rule
@@ -30,7 +31,7 @@ public class JsPlayerTest {
 
     @Test
     public void onUnparsableReinforcementMoveReturnsEmptyList() {
-        String script = "function onReinforcement(board, soldiers) { return [{'adom':'yes', 'yarok':'yes', 'garinimShelAvatiach':'yes'}]; }";
+        String script = "function " + REINFORCEMENT_JS_FUNCTION_NAME + "(board, soldiers) { return [{'adom':'yes', 'yarok':'yes', 'garinimShelAvatiach':'yes'}]; }";
         Player player = new JsPlayer(script);
         Iterable<ReinforcementMove> moves = player.onReinforcement(board, 0);
         assertFalse(moves.iterator().hasNext());
@@ -38,7 +39,7 @@ public class JsPlayerTest {
 
     @Test
     public void onUnparsableAttackMoveReturnsEmptyList() {
-        String script = "function onAttack(board) { return [{'hamburger': 'sandwich', 'hotdog':'sandwich', 'burrito':'not_sandwich'}]; }";
+        String script = "function " + ATTACK_JS_FUNCTION_NAME + "(board) { return [{'hamburger': 'sandwich', 'hotdog':'sandwich', 'burrito':'not_sandwich'}]; }";
         Player player = new JsPlayer(script);
         Iterable<AttackMove> moves = player.onAttack(board);
         assertFalse(moves.iterator().hasNext());
@@ -46,8 +47,7 @@ public class JsPlayerTest {
 
     @Test
     public void playerReturnsReinforcementMoves() {
-        String onReinforcement = JsPlayer.REINFORCEMENT_JS_FUNCTION_NAME;
-        String script = "function " + onReinforcement + "(board, soldiers) { return [{'col':1, 'row':2, 'amount':5}]; }";
+        String script = "function " + REINFORCEMENT_JS_FUNCTION_NAME + "(board, soldiers) { return [{'col':1, 'row':2, 'amount':5}]; }";
         Player player = new JsPlayer(script);
         Iterable<ReinforcementMove> moves = player.onReinforcement(board, 0);
         assertEquals(new ReinforcementMove(1, 2, 5), moves.iterator().next());
@@ -55,8 +55,7 @@ public class JsPlayerTest {
 
     @Test
     public void playerReturnsAttackMoves() {
-        String onAttack = JsPlayer.ATTACK_JS_FUNCTION_NAME;
-        String script = "function " + onAttack + "(board) { return [{'originCol':1, 'originRow':2,'destCol':3, 'destRow':4, 'amount':5}]; }";
+        String script = "function " + ATTACK_JS_FUNCTION_NAME + "(board) { return [{'originCol':1, 'originRow':2,'destCol':3, 'destRow':4, 'amount':5}]; }";
         Player player = new JsPlayer(script);
         Iterable<AttackMove> moves = player.onAttack(board);
         assertEquals(new AttackMove(1, 2, 3, 4, 5), moves.iterator().next());
