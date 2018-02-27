3bbcfada526be8f0cfc7c54882310caee5abe175
IdoAgam <teamagam2@gmail.com>
2018-02-27 15:07:30 +0200

    Improved logger's messages

--- src/main/java/rasos/StdoutRiskLogger.java
+++ src/main/java/rasos/StdoutRiskLogger.java
@@ -2,7 +2,7 @@ package rasos;
 
 public class StdoutRiskLogger implements RiskLogger {
 
-    public static final String ASOS = "\n" +
+    private static final String ASOS = "\n" +
             "  __ _ ___  ___  ___ \n" +
             " / _` / __|/ _ \\/ __|\n" +
             "| (_| \\__ \\ (_) \\__ \\\n" +
@@ -14,34 +14,36 @@ public class StdoutRiskLogger implements RiskLogger {
         this(System.out::println);
     }
 
-    public StdoutRiskLogger(Printer printer) {
+    private StdoutRiskLogger(Printer printer) {
         this.printer = printer;
     }
 
     @Override
     public void logGameStart() {
         printer.print(ASOS);
-        printer.print(("Game started"));
+        printer.print(("Game started!"));
     }
 
     @Override
     public void logSuccessfulReinforcement(int playerId, ReinforcementMove move) {
-        printer.print((String.format("player %d executes reinforcement move %s", playerId, move)));
+        printer.print((String.format("player %d reinforced %d soldier(s) in %d,%d",
+                playerId, move.getAmount(), move.getRow(), move.getCol())));
     }
 
     @Override
     public void logFailedReinforcement(int playerId, ReinforcementMove move) {
-        printer.print(String.format("player %d failed to execute reinforcement move %s", playerId, move));
+        printer.print(String.format("player %d failed to reinforce (move: %s)", playerId, move));
     }
 
     @Override
     public void logSuccessfulAttack(int playerId, AttackMove move) {
-        printer.print(String.format("player %d executes attack move %s", playerId, move));
+        printer.print(String.format("player %d attacked from %d,%d to %d,%d with %d soldier(s)",
+                playerId, move.getOriginRow(), move.getOriginCol(), move.getDestRow(), move.getDestCol(), move.getAmount()));
     }
 
     @Override
     public void logFailedAttack(int playerId, AttackMove move) {
-        printer.print(String.format("player %d failed to execute attack move %s", playerId, move));
+        printer.print(String.format("player %d failed to attack (move: %s)", playerId, move));
     }
 
     @Override
