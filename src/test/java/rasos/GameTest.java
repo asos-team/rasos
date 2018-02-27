beda249f084a5326d45e9845a8e1012870c4aebc
Guy Bukchin <sevalor@gmail.com>
2018-02-27 15:12:50 +0200

    Shitty JsPlayer test exists

--- src/test/java/rasos/GameTest.java
+++ src/test/java/rasos/GameTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.mockito.InOrder;
 import rasos.players.AttackPlayer;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.util.concurrent.Executors;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -34,6 +36,28 @@ public class GameTest {
         RiskLogger logger = new StdoutRiskLogger(s -> {});
         RoundHandler handler = new RoundHandler(
                 new AttackPlayer(),
+                new AttackPlayer(),
+                new Reinforcer(logger),
+                new Attacker(logger),
+                Executors.newSingleThreadExecutor(),
+                logger);
+        Game g = new Game(5, 20, 50, handler, new GameEndChecker(), logger);
+
+        g.start();
+    }
+
+    @Test
+    public void gameWithShittyJsPlayer(){
+        RiskLogger logger = new StdoutRiskLogger();
+        String script = "function onReinforcement(board, reinforcement){" +
+                "return [{col:1,row:1,amount:reinforcement+board.cellAt(1,1).getNumSoldiers()+board.cellAt(5,5).getNumSoldiers()}];" +
+                "}; function onAttack(board){" +
+                "return [];};";
+
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
+        JsonParser parser = new JsonParser();
+        RoundHandler handler = new RoundHandler(
+                new JsPlayer(script,engine,parser),
                 new AttackPlayer(),
                 new Reinforcer(logger),
                 new Attacker(logger),
